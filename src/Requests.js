c5d9553df9104ef4bc6b249092a73f53c2a8ae79
north-tower <muneshmyke@gmail.com>
2023-05-09 15:12:01 +0300

    tmdb

--- src/Requests.js
+++ src/Requests.js
@@ -1,2 +1,17 @@
 const API_KEY = '39dffe0557c7207907d85d373967b62d';
 
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanticMovies: `/discover/movies?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movies?api_key=${API_KEY}&with_genres=99`,
+
+};
+
+
+
+export default requests;
